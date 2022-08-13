5c34497b2cd55ea3b5119efb869fd08e1e6d6399
Ilknur Yilmaz <ilknuray1318@gmail.com>
2022-08-13 21:37:16 +0200

    C.4

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,6 +2,8 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Git 1. Deneme");
         System.out.println("Hello Git 2. Deneme");
-        System.out.println("Hello Git 3.Deneme");
+        System.out.println("Hello Git 3. Deneme");
+        System.out.println("Hello Git 4. Deneme");
+
     }
 }
