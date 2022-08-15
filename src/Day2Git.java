c0dc0b5128e60c01dc95e426e4177e4544563da3
Ilknur Yilmaz <ilknuray1318@gmail.com>
2022-08-15 20:36:40 +0200

    V4

--- src/Day2Git.java
+++ src/Day2Git.java
@@ -1,5 +1,5 @@
 public class Day2Git {
     public static void main(String[] args) {
-        System.out.println("yapacagima inaniyorum");
+        System.out.println("yapacagima inaniyorum;;;");
     }
 }
